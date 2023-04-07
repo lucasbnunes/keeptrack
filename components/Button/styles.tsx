f11690ad52d0fb0d229215a31acfc9413897bf06
lucasbnunes <lucasbnunes23@gmail.com>
2023-04-07 15:19:19 -0300

    feat: create base button component

---
+++ components/Button/styles.tsx
@@ -0,0 +1,28 @@
+import styled, { DefaultTheme, keyframes } from "styled-components";
+
+export const TextButton = styled.button`
+  background: none;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  border-radius: 4px;
+  transition: background 0.2s ease-in-out;
+  font-weight: 600;
+
+  &:hover {
+    background: ${({ theme }) => theme.colors.gray[50]}
+  }
+
+  &:active {
+    background: ${({ theme }) => theme.colors.gray[100]}
+  }
+
+  & svg {
+    width: 1.25rem;
+    height: 1.25rem;
+  }
+`
