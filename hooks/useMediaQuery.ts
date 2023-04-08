41101564c1a2c6678dc13e29a235a31356a856a5
lucasbnunes <lucasbnunes23@gmail.com>
2023-04-08 14:11:39 -0300

    feat: create useMediaQuery hook

---
+++ hooks/useMediaQuery.ts
@@ -0,0 +1,49 @@
+import { theme } from '@/styles/theme';
+import { useEffect, useState } from 'react';
+import { DefaultTheme } from 'styled-components';
+
+interface WindowDimensions {
+  width?: number;
+  height?: number;
+}
+
+type Breakpoints = keyof DefaultTheme['breakpoints'];
+
+export function useMediaQuery() {
+  const [windowSize, setWindowSize] = useState<WindowDimensions>({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  function minWidth(breakpoint: Breakpoints) {
+    if (windowSize.width) {
+      return windowSize.width >= parseInt(theme.breakpoints[breakpoint]);
+    }
+  }
+
+  function maxWidth(breakpoint: Breakpoints) {
+    if (windowSize.width) {
+      return windowSize.width < parseInt(theme.breakpoints[breakpoint]);
+    }
+  }
+
+  return {
+    windowSize,
+    minWidth,
+    maxWidth,
+  };
+}
