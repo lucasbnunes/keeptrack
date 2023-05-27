4a6c9d41bc7310d1de61fbab5dafa7059398a4f5
lucasbnunes <lucasbnunes23@gmail.com>
2023-05-27 18:30:28 -0300

    feat: add update application modal

--- features/applications/ApplicationsTable/index.tsx
+++ features/applications/ApplicationsTable/index.tsx
@@ -3,6 +3,8 @@ import { StatusChip } from "../StatusChip";
 import { ApplicationsTableBody, ApplicationsTableHead, ApplicationsTableRoot, ApplicationsVerticalCell } from "./style";
 import { useDateFormat } from "@/hooks/useDateFormat";
 import { Application } from "@prisma/client";
+import { UpdateApplicationModal } from "../UpdateApplicationModal";
+import { useState } from "react";
 
 interface ApplicationTableProps {
   applications: Application[]
@@ -10,32 +12,44 @@ interface ApplicationTableProps {
 
 export function ApplicationsTable({ applications }: ApplicationTableProps) {
   const { formatToRelativeDate } = useDateFormat()
+  const [openUpdateModal, setOpenUpdateModal] = useState(false)
+  const [applicationBeingEdited, setApplicationBeingEdited] = useState<Application | null>(null);
+
+  function handleClickEdit(application: Application) {
+    setOpenUpdateModal(true)
+    setApplicationBeingEdited(application)
+  }
 
   return (
-    <ApplicationsTableRoot>
-      <ApplicationsTableHead>
-        <tr>
-          <th>Position</th>
-          <th>Status</th>
-          <th>Applied</th>
-          <th></th>
-        </tr>
-      </ApplicationsTableHead>
-
-      <ApplicationsTableBody>
-        {applications.map((application) => (
-          <tr key={application.id}>
-            <ApplicationsVerticalCell>
-              <span>{application.title}</span>
-              <span>{application.company}</span>
-            </ApplicationsVerticalCell>
-            <td><StatusChip status={application.status} /></td>
-            <td>{formatToRelativeDate(new Date(application.applicationDate))}</td>
-            <td><Button>Details</Button></td>
+    <>
+      {applicationBeingEdited && openUpdateModal &&
+        <UpdateApplicationModal application={applicationBeingEdited} open={openUpdateModal} setOpen={setOpenUpdateModal} />
+      }
+      <ApplicationsTableRoot>
+        <ApplicationsTableHead>
+          <tr>
+            <th>Position</th>
+            <th>Status</th>
+            <th>Applied</th>
+            <th></th>
           </tr>
-        ))}
+        </ApplicationsTableHead>
 
-      </ApplicationsTableBody>
-    </ApplicationsTableRoot>
+        <ApplicationsTableBody>
+          {applications.map((application) => (
+            <tr key={application.id}>
+              <ApplicationsVerticalCell>
+                <span>{application.title}</span>
+                <span>{application.company}</span>
+              </ApplicationsVerticalCell>
+              <td><StatusChip status={application.status} /></td>
+              <td>{formatToRelativeDate(new Date(application.applicationDate))}</td>
+              <td><Button onClick={() => handleClickEdit(application)}>Edit</Button></td>
+            </tr>
+          ))}
+
+        </ApplicationsTableBody>
+      </ApplicationsTableRoot>
+    </>
   )
 }
