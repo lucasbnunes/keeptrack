6e359625d98d98622e37efe41562e706b97759da
lucasbnunes <lucasbnunes23@gmail.com>
2023-05-27 19:22:55 -0300

    fix: remove style file from the pages directory

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
-import { Inter } from 'next/font/google'
-import { signIn, signOut, useSession } from 'next-auth/react'
+import { signIn } from 'next-auth/react'
 import { Button } from '@/components/Button'
-import { Container, Heading, Hero, LogoContainer } from './style'
 import { Logo } from '@/components/Logo'
+import styled from 'styled-components'
 
 
 export default function Home() {
@@ -21,4 +20,50 @@ export default function Home() {
     </Container>
   )
 }
+
+const Container = styled.div`
+  background: white;
+  color: ${({ theme }) => theme.colors.gray[600]};
+  font-size: 1.125rem;
+
+  height: 100vh;
+  padding: 0 24px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`;
+
+const LogoContainer = styled.div`
+  position: absolute;
+  top: 0;
+  padding: 0.75rem 1rem;
+  max-width: 1280px;
+
+  & > img {
+    max-width: 160px;
+  }
+`;
+
+const Hero = styled.div`
+  max-width: 1280px;
+  margin: 0 auto;
+  text-align: center;
+
+  & button {
+    margin: 2rem auto;
+    & > img {
+      width: 24px;
+      height: 24px;
+    }
+  }
+`;
+
+const Heading = styled.h1`
+  text-align: center;
+  font-size: 2.5rem;
+  margin-bottom: 24px;
+  line-height: calc(2.5rem + 10px);
+  color: ${({ theme }) => theme.colors.blue[600]};
+`;
 
