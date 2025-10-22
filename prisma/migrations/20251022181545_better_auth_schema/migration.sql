7cdbe524ff6e02b5fa9b8ae680d825a9ae0d5411
Lucas Brito <lucasbnunes23@gmail.com>
2025-10-22 15:26:21 -0300

    fix: update db schema to work with better-auth

---
+++ prisma/migrations/20251022181545_better_auth_schema/migration.sql
@@ -0,0 +1,45 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `access_token` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `expires_at` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `id_token` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `provider` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `providerAccountId` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `refresh_token` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `session_state` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `token_type` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `type` on the `account` table. All the data in the column will be lost.
+  - You are about to drop the column `expires` on the `session` table. All the data in the column will be lost.
+  - You are about to drop the column `sessionToken` on the `session` table. All the data in the column will be lost.
+  - Made the column `name` on table `user` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `email` on table `user` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `emailVerified` on table `user` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- DropIndex
+DROP INDEX "public"."account_provider_providerAccountId_key";
+
+-- DropIndex
+DROP INDEX "public"."session_sessionToken_key";
+
+-- AlterTable
+ALTER TABLE "account" DROP COLUMN "access_token",
+DROP COLUMN "expires_at",
+DROP COLUMN "id_token",
+DROP COLUMN "provider",
+DROP COLUMN "providerAccountId",
+DROP COLUMN "refresh_token",
+DROP COLUMN "session_state",
+DROP COLUMN "token_type",
+DROP COLUMN "type";
+
+-- AlterTable
+ALTER TABLE "session" DROP COLUMN "expires",
+DROP COLUMN "sessionToken";
+
+-- AlterTable
+ALTER TABLE "user" ALTER COLUMN "name" SET NOT NULL,
+ALTER COLUMN "email" SET NOT NULL,
+ALTER COLUMN "emailVerified" SET NOT NULL,
+ALTER COLUMN "emailVerified" SET DEFAULT false;
