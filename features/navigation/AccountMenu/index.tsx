8d98e8d5a1ff23dab3c45e4559cdda924d31c517
Lucas Brito <lucasbnunes23@gmail.com>
2025-10-22 16:10:06 -0300

    refactor: rewrite sign out handler to use better-auth

--- features/navigation/AccountMenu/index.tsx
+++ features/navigation/AccountMenu/index.tsx
@@ -1,25 +1,35 @@
-import { Avatar } from '@/components/Avatar';
-import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
-import { AccountButton, StyledDropdownMenuContent, StyledDropdownMenuItem } from './styles';
-import { ExitIcon } from '@radix-ui/react-icons'
-import { signOut, useSession } from 'next-auth/react';
+import { Avatar } from "@/components/Avatar";
+import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
+import {
+  AccountButton,
+  StyledDropdownMenuContent,
+  StyledDropdownMenuItem,
+} from "./styles";
+import { ExitIcon } from "@radix-ui/react-icons";
+import { authClient } from "@/lib/auth-client";
+import { redirect } from "next/navigation";
 
 export function AccountMenu() {
+  async function handleSignOut() {
+    await authClient.signOut();
+    redirect("/");
+  }
+
   return (
     <DropdownMenu.Root>
       <DropdownMenu.Trigger asChild>
-        <AccountButton aria-label='account menu'>
+        <AccountButton aria-label="account menu">
           <Avatar />
         </AccountButton>
       </DropdownMenu.Trigger>
 
       <DropdownMenu.Portal>
         <StyledDropdownMenuContent sideOffset={5} collisionPadding={20}>
-          <StyledDropdownMenuItem onClick={() => signOut()}>
+          <StyledDropdownMenuItem onClick={handleSignOut}>
             Sign out <ExitIcon />
           </StyledDropdownMenuItem>
         </StyledDropdownMenuContent>
       </DropdownMenu.Portal>
     </DropdownMenu.Root>
-  )
+  );
 }
