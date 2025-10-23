bb113c797ce3a587bbc87fbbfd0b51daee506664
Lucas Brito <lucasbnunes23@gmail.com>
2025-10-23 11:10:13 -0300

    refactor: migrate auth api route to app folder

--- src/pages/api/auth/[...all].ts
+++
@@ -1,6 +0,0 @@
-import { toNodeHandler } from "better-auth/node";
-import { auth } from "@/lib/auth";
-
-export const config = { api: { bodyParser: false } };
-
-export default toNodeHandler(auth.handler);
