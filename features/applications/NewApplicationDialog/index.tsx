f718bad1478ef459641c92193305b3871596f156
Lucas Brito <lucasbnunes23@gmail.com>
2025-10-24 15:09:41 -0300

    refactor: use the new dialog component

---
+++ features/applications/NewApplicationDialog/index.tsx
@@ -0,0 +1,117 @@
+"use client";
+
+import { Input } from "@/components/Input";
+// import { Modal } from "@/components/Modal";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogOverlay,
+  DialogPortal,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/Dialog";
+import { NewApplicationForm } from "./style";
+import { Button } from "@/components/Button";
+import { useForm } from "react-hook-form";
+import { SubmitHandler } from "react-hook-form/dist/types";
+import {
+  NewApplication,
+  useApplicationMutation,
+} from "../useApplicationMutation";
+import { useState } from "react";
+import { PlusIcon } from "@radix-ui/react-icons";
+
+const tzOffsetInMilliseconds = new Date().getTimezoneOffset() * 60000;
+const DEFAULT_APPLICATION_DATE = new Date(Date.now() - tzOffsetInMilliseconds)
+  .toISOString()
+  .slice(0, 10);
+
+export function NewApplicationModal() {
+  const [open, setOpen] = useState(false);
+  const applicationMutation = useApplicationMutation();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<NewApplication>();
+
+  const onSubmit: SubmitHandler<NewApplication> = async (data) => {
+    try {
+      await applicationMutation.mutateAsync({ ...data, status: "applied" });
+      setOpen(false);
+      reset();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        <Button onClick={() => setOpen(true)} variant="solid">
+          <PlusIcon /> New application
+        </Button>
+      </DialogTrigger>
+      <DialogPortal>
+        <DialogOverlay />
+        <DialogContent>
+          <DialogClose onClick={() => setOpen(false)} />
+          <DialogTitle>New Application</DialogTitle>
+          <NewApplicationForm
+            id="applicationForm"
+            onSubmit={handleSubmit(onSubmit)}
+          >
+            <Input
+              label="Job title"
+              fullWidth
+              inputProps={{
+                ...register("title", { required: true }),
+                disabled: applicationMutation.isLoading,
+              }}
+            />
+            <Input
+              label="Company"
+              fullWidth
+              inputProps={{
+                ...register("company", { required: true }),
+                disabled: applicationMutation.isLoading,
+              }}
+            />
+            <Input
+              label="Application date"
+              inputProps={{
+                ...register("applicationDate", { required: true }),
+                type: "date",
+                defaultValue: DEFAULT_APPLICATION_DATE,
+                disabled: applicationMutation.isLoading,
+              }}
+              fullWidth
+            />
+            <Input
+              label="Notes"
+              multiline
+              fullWidth
+              inputProps={{
+                ...register("notes"),
+                disabled: applicationMutation.isLoading,
+              }}
+            />
+          </NewApplicationForm>
+          <DialogFooter>
+            <Button
+              type="submit"
+              form="applicationForm"
+              loading={applicationMutation.isLoading}
+            >
+              {" "}
+              Save
+            </Button>
+          </DialogFooter>
+        </DialogContent>
+      </DialogPortal>
+    </Dialog>
+  );
+}
