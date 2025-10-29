3f7e51a36c0f1d791f41b0611a91f5524bf976be
Lucas Brito <lucasbnunes23@gmail.com>
2025-10-29 11:34:25 -0300

    feat: replace search input

--- src/features/applications/SearchForm/index.tsx
+++ src/features/applications/SearchForm/index.tsx
@@ -1,20 +1,24 @@
 "use client";
 
-import { SearchInput } from "@/components/SearchInput";
+import { Button } from "@/components/ui/button";
+import { Field, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { ChangeEvent } from "react";
-import { ApplicationsSearchForm } from "./style";
+import { FormEvent, useState } from "react";
 
 const paramName = "search";
+const placeholder = "Company or job title";
 
 export function SearchForm() {
+  const [search, setSearch] = useState("");
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(e: ChangeEvent<HTMLInputElement>) {
+  function handleSearch(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
     const params = new URLSearchParams(searchParams || {});
-    const value = e.target.value;
+    const value = search;
 
     if (value) {
       params.set(paramName, value);
@@ -26,11 +30,27 @@ export function SearchForm() {
   }
 
   return (
-    <ApplicationsSearchForm>
-      <SearchInput
-        placeholder="Search by company or job title"
-        inputProps={{ onChange: handleSearch }}
-      />
-    </ApplicationsSearchForm>
+    <form
+      className="my-6 flex items-center gap-2 max-w-sm"
+      onSubmit={handleSearch}
+    >
+      <Field>
+        <FieldLabel
+          aria-label={placeholder}
+          htmlFor={paramName}
+          className="hidden"
+        />
+        <Input
+          id={paramName}
+          name={paramName}
+          placeholder={placeholder}
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </Field>
+      <Button variant="secondary" type="submit">
+        Search
+      </Button>
+    </form>
   );
 }
