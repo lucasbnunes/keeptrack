4b0618dcdb3e57f606c8bf1857814b8bbbda4f8f
Lucas Brito <lucasbnunes23@gmail.com>
2025-11-19 10:52:58 -0300

    feat: update schemas

--- src/features/applications/schemas.ts
+++ src/features/applications/schemas.ts
@@ -1,27 +1,23 @@
 import { Application, Status } from '@prisma/client';
 import z from 'zod';
 
-export type CreateApplicationFields = Pick<
-  Application,
-  'title' | 'applicationDate' | 'company' | 'notes' | 'status'
->;
+export const createApplicationSchema = z.object({
+  title: z.string().min(1, 'Title is required'),
+  company: z.string().min(1, 'Company is required'),
+  notes: z.string().optional(),
+  status: z.enum(Status),
+  applicationDate: z
+    .string()
+    .transform((dateAsString) => new Date(dateAsString)),
+  jobUrl: z.url().optional(),
+});
 
-export const createApplicationSchema: z.ZodType<CreateApplicationFields> =
-  z.object({
-    title: z.string().min(1),
-    company: z.string().min(1),
-    notes: z.string().nullable(),
-    status: z.enum(Status),
-    applicationDate: z
-      .string()
-      .transform((dateAsString) => new Date(dateAsString)),
-    jobUrl: z.url().nullable(),
-  });
+export type CreateApplicationFields = z.output<typeof createApplicationSchema>;
 
 export const updateApplicationSchema = z.object({
   id: z.string().min(1),
-  title: z.string().min(1).optional(),
-  company: z.string().min(1).optional(),
+  title: z.string().min(1, 'Title is required').optional(),
+  company: z.string().min(1, 'Company is required').optional(),
   notes: z.string().nullable().optional(),
   status: z.enum(Status).optional(),
   applicationDate: z
